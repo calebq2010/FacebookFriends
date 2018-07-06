904e427f160d3d549ad4063afdb3a4fa60a74463
Caleb Quinn <calebq2010@gmail.com>
2018-07-06 19:55:23 -0400

    My first commit

---
+++ FacebookFriends/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookFriends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prompt = "Please enter the names that liked your Facebook Post:";
+            var answer = "";
+
+            Console.WriteLine(prompt);
+            answer = Console.ReadLine();
+
+            if (answer == "")
+            {
+                Console.WriteLine("Display: ");
+            }else
+            {
+                Console.WriteLine("Display: " + answer + " Likes your post");
+            }
+            
+
+        }
+    }
+}
